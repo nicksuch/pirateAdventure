90a8bfc87dd12a1a7c2e9161a8112abd4ee50756
Luke <ldecastro2601@gmail.com>
2014-10-16 22:43:40 -0400

    Initial Commit

---
+++ Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Luke de Castro on 10/16/14.
+//  Copyright (c) 2014 Luke de Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Pirate Adventure
+//
+//  Created by Luke de Castro on 10/16/14.
+//  Copyright (c) 2014 Luke de Castro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
